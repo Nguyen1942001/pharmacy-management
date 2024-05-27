0e85ad90559514c57d4229ca7835e74cf90bacaa
Khoi Nguyen <khoinguyen1942001@gmail.com>
2024-05-27 16:50:04 +0700

    fix layout login

--- admin/views/login/index.tsx
+++ admin/views/login/index.tsx
@@ -6,6 +6,7 @@ import Image from 'next/image';
 // import antd
 import { Button, Checkbox, Col, ConfigProvider, Form, FormProps, Input, Row } from 'antd';
 import { TinyColor } from '@ctrl/tinycolor';
+import Link from 'next/link';
 
 type FieldType = {
   email?: string;
@@ -35,13 +36,13 @@ const LoginPage = () => {
             alt='Picture of the author'
             fill={true}
             sizes='100%'
-            className='object-cover'
+            className='rounded-xl object-cover'
             priority={true}
           />
         </Col>
 
         <Col span={8}>
-          <div className='p-14'>
+          <div className='flex h-full w-full flex-col items-center justify-center bg-pink-50 p-14'>
             <div className='mb-4 text-center'>
               <Image
                 src='/images/logo_pharmacy.png'
@@ -57,75 +58,71 @@ const LoginPage = () => {
 
             <div className='mb-1 text-center'>Chào mừng bạn quay trở lại nhà thuốc</div>
 
-            <div>
-              <Form
-                name='form_login'
-                layout='vertical'
-                labelCol={{ span: 24 }}
-                wrapperCol={{ span: 24 }}
-                style={{ maxWidth: 600 }}
-                initialValues={{ remember: true }}
-                size='large'
-                onFinish={onFinish}
-                onFinishFailed={onFinishFailed}
-                autoComplete='off'
-                requiredMark={false}
+            <Form
+              name='form_login'
+              layout='vertical'
+              labelCol={{ span: 24 }}
+              wrapperCol={{ span: 24 }}
+              style={{ minWidth: '100%' }}
+              initialValues={{ remember: true }}
+              size='large'
+              onFinish={onFinish}
+              onFinishFailed={onFinishFailed}
+              autoComplete='off'
+              requiredMark={false}
+            >
+              <Form.Item<FieldType>
+                label='Email'
+                name='email'
+                rules={[{ required: true, message: 'Hãy nhập email!' }]}
+                style={{ marginBottom: 10 }}
               >
-                <Form.Item<FieldType>
-                  label='Email'
-                  name='email'
-                  rules={[{ required: true, message: 'Hãy nhập email!' }]}
-                  style={{ marginBottom: 10 }}
-                >
-                  <Input placeholder='abc@gmail.com' />
-                </Form.Item>
+                <Input placeholder='abc@gmail.com' />
+              </Form.Item>
 
-                <Form.Item<FieldType>
-                  label='Mật khẩu'
-                  name='password'
-                  rules={[{ required: true, message: 'Hãy nhập mật khẩu!' }]}
-                  style={{ marginBottom: 10 }}
-                >
-                  <Input.Password placeholder='Mật khẩu' />
-                </Form.Item>
+              <Form.Item<FieldType>
+                label='Mật khẩu'
+                name='password'
+                rules={[{ required: true, message: 'Hãy nhập mật khẩu!' }]}
+                style={{ marginBottom: 10 }}
+              >
+                <Input.Password placeholder='Mật khẩu' />
+              </Form.Item>
 
-                <Form.Item wrapperCol={{ offset: 0, span: 24 }}>
-                  <Row justify='space-between'>
-                    <Col>
-                      <Form.Item name='remember' valuePropName='checked' noStyle>
-                        <Checkbox>Ghi nhớ đăng nhập</Checkbox>
-                      </Form.Item>
-                    </Col>
+              <Form.Item wrapperCol={{ offset: 0, span: 24 }}>
+                <Row justify='space-between'>
+                  <Col>
+                    <Form.Item name='remember' valuePropName='checked' noStyle>
+                      <Checkbox>Ghi nhớ đăng nhập</Checkbox>
+                    </Form.Item>
+                  </Col>
 
-                    <Col>
-                      <a className='' href=''>
-                        Quên mật khẩu
-                      </a>
-                    </Col>
-                  </Row>
-                </Form.Item>
+                  <Col>
+                    <Link href='/admin/forgot-password'>Quên mật khẩu</Link>
+                  </Col>
+                </Row>
+              </Form.Item>
 
-                {/* Button */}
-                <Form.Item>
-                  <ConfigProvider
-                    theme={{
-                      components: {
-                        Button: {
-                          colorPrimary: `linear-gradient(135deg, ${colors1.join(', ')})`,
-                          colorPrimaryHover: `linear-gradient(135deg, ${getHoverColors(colors1).join(', ')})`,
-                          colorPrimaryActive: `linear-gradient(135deg, ${getActiveColors(colors1).join(', ')})`,
-                          lineWidth: 0,
-                        },
+              {/* Button */}
+              <Form.Item>
+                <ConfigProvider
+                  theme={{
+                    components: {
+                      Button: {
+                        colorPrimary: `linear-gradient(135deg, ${colors1.join(', ')})`,
+                        colorPrimaryHover: `linear-gradient(135deg, ${getHoverColors(colors1).join(', ')})`,
+                        colorPrimaryActive: `linear-gradient(135deg, ${getActiveColors(colors1).join(', ')})`,
+                        lineWidth: 0,
                       },
-                    }}
-                  >
-                    <Button type='primary' htmlType='submit' size='large' block={true}>
-                      Đăng nhập
-                    </Button>
-                  </ConfigProvider>
-                </Form.Item>
-              </Form>
-            </div>
+                    },
+                  }}
+                >
+                  <Button type='primary' htmlType='submit' size='large' block={true}>
+                    Đăng nhập
+                  </Button>
+                </ConfigProvider>
+              </Form.Item>
+            </Form>
           </div>
         </Col>
       </Row>
